84f73d4999ee189ddd2d13f1e560a8c28c02c2f2
Dominic <55569343+doominkim@users.noreply.github.com>
2024-03-18 11:25:03 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,5 +1,10 @@
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().split(' ');
-var a = parseInt(input[0]);
-var b = parseInt(input[1]);
-console.log(a+b);
+const fs = require('fs');
+const INPUT = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+function solution() {
+    var a = parseInt(INPUT[0]);
+    var b = parseInt(INPUT[1]);
+    console.log(a+b);
+}
+
+solution();
