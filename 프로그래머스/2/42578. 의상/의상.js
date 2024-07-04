6c72547f220adb6d8956931fdb1c0b3e679d56d0
Dominic <55569343+doominkim@users.noreply.github.com>
2024-07-04 15:59:53 +0900

    [level 2] Title: 의상, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,3 +1,8 @@
+// TODO DP 접근법으로 다시 풀어보자.
+// DP 접근법
+// dp[N][K] : N개의 종류 중 K개를 선택하는 경우의 수 (K <= N)
+// dp[N][K] = dp[N - 1][K] + dp[N - 1][K - 1] * cnt[N]
+
 function solution(clothes) {
     let clotheTable = {}
     for(const clothe of clothes) {
@@ -7,10 +12,12 @@ function solution(clothes) {
         clotheTable[clothe[1]].add(clothe[0])
     }
     
-    let size = 0
+    
+    // 수학적 접근법 
     let gop = 1
     for(const key in clotheTable) {
         gop *= clotheTable[key].size + 1
     }
+    
     return gop - 1
 }
