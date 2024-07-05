d70f8f86b3e329997d35b809d1059a36dc9b2dbe
Dominic <55569343+doominkim@users.noreply.github.com>
2024-07-05 16:21:17 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 38.36 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,27 @@
+function solution(n, m) {
+    const minimumFunc = (number) => {
+        const list = [];
+        for(let i=0; i<=number; i++) {
+            if(number % i === 0) list.push(i);
+        }
+        return list;
+    }
+    const minimum = minimumFunc(n).filter((e) => minimumFunc(m).includes(e)).pop();
+    
+    const maximumFunc = (n, m) => {
+        const list1 = [];
+        const list2 = [];
+        
+        let cnt = 0;
+        while(true) {
+            cnt++;
+            list1.push(n * cnt);
+            list2.push(m * cnt);
+            if(list1.find((item) => list2.includes(item))) break;
+        }
+        
+        return list1[list1.length - 1] > list2[list2.length - 1] ? list2.pop() : list1.pop();
+    }
+    
+    return [minimum, maximumFunc(n, m)];
+}
