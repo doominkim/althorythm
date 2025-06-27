12b1571ec9b6c69e16a49b3624f86e2c30665d20
Dominic <55569343+doominkim@users.noreply.github.com>
2025-06-27 14:59:19 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.ts
@@ -0,0 +1,13 @@
+function countConsistentStrings(allowed: string, words: string[]): number {
+    const regexp = new RegExp(`/[^${allowed}]/i`);
+    
+    let count = 0;
+    for(const word of words) {
+        console.log(word.search(regexp))
+        if(word.search(regexp) < 0) {
+            count++
+        }
+    }
+
+    return count
+};
