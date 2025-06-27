c42198a6b4aedb8721c5717d3c30fb8ed9d4ba56
Dominic <55569343+doominkim@users.noreply.github.com>
2025-06-27 15:19:11 +0900

    Time: 0 ms (100%), Space: 56.2 MB (67.6%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
@@ -1,4 +1,4 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
     const maxCandy = Math.max(...candies)
-    return candies.map((candy) => (candy + extraCandies) >= maxCandy ? true: false)
+    return candies.map((candy) => (candy + extraCandies) >= maxCandy)
 };
