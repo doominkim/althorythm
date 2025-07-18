448c44b6869466b781819ccb147b094ba1ea6753
Dominic <55569343+doominkim@users.noreply.github.com>
2025-07-18 18:30:12 +0900

    Time: 41 ms (19.05%), Space: 67.3 MB (9.52%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.ts
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.ts
@@ -1,6 +1,5 @@
 function numberOfBeams(bank: string[]): number {
     const doors = bank.map((row) => row.match(/1/g)?.length ?? 0).filter((row) => row > 0)
-
     let lazerBeamCount = 0;
     for(let i=0; i<doors.length - 1; i++) {
         lazerBeamCount += doors[i] * doors[i+1]
