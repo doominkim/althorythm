c55da5ade918a738428496bc41263c7ef33fa222
Dominic <55569343+doominkim@users.noreply.github.com>
2025-07-18 18:08:58 +0900

    Time: 1 ms (36.36%), Space: 54.1 MB (81.82%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.ts
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.ts
@@ -1,21 +1,32 @@
 function smallestNumber(pattern: string): string {
-    const availableNumbers = Array.from(Array(pattern.length+1), (_, i) => i+1);
-    let stack = []
-    const result = []
-
-    for(let i=0; i<pattern.length; i++) {
-        if(pattern[i] === 'I') {
-            result.push(availableNumbers.shift())
-            result.push(...stack.reverse())
-            stack = []
-        } else {
-            stack.push(availableNumbers.shift())
+    const stack: number[] = [];
+    let result = '';
+    let num = 1;
+    for (let i = 0; i <= pattern.length; i++) {
+        stack.push(num++);
+        if (i === pattern.length || pattern[i] === 'I') {
+            //if exist stack values
+            while (stack.length) {
+                result += stack.pop();
+            }
         }
-
-        console.log(availableNumbers, pattern[i], result, stack)
     }
+    return result;
+    // const availableNumbers = Array.from(Array(pattern.length+1), (_, i) => i+1);
+    // let stack = []
+    // const result = []
 
-    result.push(...availableNumbers, ...stack.reverse())
+    // for(let i=0; i<pattern.length; i++) {
+    //     if(pattern[i] === 'I') {
+    //         result.push(availableNumbers.shift())
+    //         result.push(...stack.reverse())
+    //         stack = []
+    //     } else {
+    //         stack.push(availableNumbers.shift())
+    //     }
+    // }
 
-    return result.join('')
+    // result.push(...availableNumbers, ...stack.reverse())
+
+    // return result.join('')
 };
